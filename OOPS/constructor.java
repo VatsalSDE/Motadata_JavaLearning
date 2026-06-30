9040e5e2069464f24c2c16584f5dd59c3b4c9491
Vatsal Rathi <ds099315@gmail.com>
2026-06-30 17:59:05 +0530

    advance java 8 features

---
+++ OOPS/constructor.java
@@ -0,0 +1,17 @@
+class A{
+    A(int i){
+        System.out.println(i);
+    }
+}
+
+class B extends A{
+    B(int i){
+        super(i);
+    }
+}
+
+public class constructor {
+    public static void main(String[] args) {
+        
+    }
+}
